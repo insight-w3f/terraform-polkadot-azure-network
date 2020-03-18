d208dc24d74e8284ecc7b7d6a5c916b066221bac
Richard Mah <dick.mah@gmail.com>
2020-03-18 12:06:34 -0400

    CIDR block is now list of strings VPC name is fixed Add count indices to subnets Fix prefix for inet route

--- vpc.tf
+++ vpc.tf
@@ -24,24 +24,24 @@ locals {
 }
 
 resource "azurerm_virtual_network" "vpc_network" {
-  address_space       = var.cidr
+  address_space       = [var.cidr]
   location            = data.azurerm_resource_group.this.location
-  name                = var.network_name
+  name                = var.vpc_name
   resource_group_name = data.azurerm_resource_group.this.name
 }
 
 resource "azurerm_subnet" "public" {
   count                = length(local.public_subnets)
-  address_prefix       = local.public_subnets
-  name                 = local.public_subnet_names
+  address_prefix       = local.public_subnets[0]
+  name                 = local.public_subnet_names[0]
   resource_group_name  = data.azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.vpc_network.name
 }
 
 resource "azurerm_subnet" "private" {
   count                = length(local.private_subnets)
-  address_prefix       = local.private_subnets
-  name                 = local.private_subnet_names
+  address_prefix       = local.private_subnets[0]
+  name                 = local.private_subnet_names[0]
   resource_group_name  = data.azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.vpc_network.name
 }
@@ -60,7 +60,7 @@ resource "azurerm_route_table" "private" {
 
 resource "azurerm_route" "public-inet" {
   count               = length(local.public_subnets)
-  address_prefix      = local.public_subnets
+  address_prefix      = "0.0.0.0/0"
   name                = "public-inet"
   next_hop_type       = "Internet"
   resource_group_name = data.azurerm_resource_group.this.name
