4f720c3959cb2db487399f6c6213bbf5f9a40fad
Richard Mah <dick.mah@gmail.com>
2020-03-28 16:47:31 -0400

    set up default test

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,4 +1,15 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "this" {
+  location = "eastus"
+  name     = "asg-default-testing"
+}
+
 
 module "defaults" {
-  source = "../.."
+  source                    = "../.."
+  azure_resource_group_name = azurerm_resource_group.this.name
 }
