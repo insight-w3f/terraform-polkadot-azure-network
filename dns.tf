8c4f7d9ff64da521e0571d8c1d63ce0156cf85f1
Richard Mah <dick.mah@gmail.com>
2020-04-10 17:55:13 -0400

    Add resources to allow Cloudflare subdomain delegation

--- dns.tf
+++ dns.tf
@@ -1,10 +1,24 @@
 locals {
-  public_domain = join(".", [data.azurerm_resource_group.this.location, var.environment, var.root_domain_name])
+  public_domain = join(".", [data.azurerm_resource_group.this.location, var.environment, "azure", var.network_name, var.root_domain_name])
+}
+
+data cloudflare_zones "this" {
+  filter {
+    name = var.root_domain_name
+  }
+}
+
+resource "cloudflare_record" "public_delegation" {
+  count   = var.root_domain_name == "" ? 0 : 4
+  name    = "azure.${var.network_name}.${var.root_domain_name}"
+  value   = flatten(azurerm_dns_zone.this[0].name_servers)[count.index]
+  type    = "NS"
+  zone_id = data.cloudflare_zones.this.zones[0].id
 }
 
 resource "azurerm_dns_zone" "this" {
   count               = var.root_domain_name == "" ? 0 : 1
-  name                = "${var.root_domain_name}."
+  name                = "azure.${var.network_name}.${var.root_domain_name}"
   resource_group_name = data.azurerm_resource_group.this.name
 }
 
@@ -25,6 +39,11 @@ resource "azurerm_dns_ns_record" "region_public" {
   name                = local.public_domain
   resource_group_name = data.azurerm_resource_group.this.name
   ttl                 = 30
-  zone_name           = azurerm_dns_zone.region_public[0].name
-  records             = [azurerm_dns_zone.region_public[0].name_servers]
+  zone_name           = azurerm_dns_zone.this[0].name
+  records = [
+    flatten(azurerm_dns_zone.region_public.*.name_servers)[0],
+    flatten(azurerm_dns_zone.region_public.*.name_servers)[1],
+    flatten(azurerm_dns_zone.region_public.*.name_servers)[2],
+    flatten(azurerm_dns_zone.region_public.*.name_servers)[3],
+  ]
 }
